c3d01aefcc3b9bcb2a6922cfe506562746ff4785
Adarley <adarleyjr@gmail.com>
2018-07-01 18:29:57 -0700

    Inicio de Pilha de Frames

---
+++ leitor_exibidor/pilhaFrames.c
@@ -0,0 +1,46 @@
+/**
+ *  @file pilhaFrames.c
+ *  @author Adarley Luiz Grando Filho (11/0007344)
+ *  @brief Codigo dos metodos declarados em pilhaFrames.h.
+ */
+
+#include "pilhaFrames.h"
+
+void init_pilha_operandos(pilha_operandos **pilha) {
+    *pilha = NULL;
+}
+
+void desaloca_pilha_operandos(pilha_operandos **pilha) {
+    pilha_operandos *op;
+
+    while (*pilha != NULL) {
+        op = *pilha;
+        *pilha = *pilha->prox;
+        free(op);
+    }
+}
+
+void empilha_operando(pilha_operandos **pilha, u4 op) {
+    pilha_operandos *operando;
+
+    operando = (pilha_operandos*)malloc(sizeof(pilha_operandos));
+    
+    operando->op = op;
+    operando->prox = *pilha;
+
+    *pilha = operando;
+}
+
+u4 desempilha_operando(pilha_operandos **pilha) {
+    u4 op;
+    pilha_operandos *elemento;
+
+    elemento = *pilha;
+
+    op = elemento->op;
+    *pilha = elemento->prox;
+
+    free(elemento);
+
+    return op;
+}
---
+++ leitor_exibidor/pilhaFrames.h
@@ -0,0 +1,49 @@
+/**
+ *  @file pilhaFrames.h
+ *  @author Adarley Luiz Grando Filho (11/0007344)
+ *  @brief Definicao da estrutura da pilha de frames e dos metodos para acessa-los.
+ */
+
+#ifndef PILHAFRAMES_H
+#define PILHAFRAMES_H
+
+#include "classfile.h"
+
+typedef struct pilha_operandos
+{
+    u4 op;
+    struct pilha_operandos *prox;
+} pilha_operandos;
+
+/**
+ *  @fn void init_pilha_operandos(pilha_operandos **pilha)
+ *  @brief Inicializa pilha de operandos setando a pilha para NULL.
+ *  @param pilha_operandos Ponteiro para ponteiro de pilha de operandos.
+ */
+ void init_pilha_operandos(pilha_operandos**);
+
+ /**
+ *  @fn void desaloca_pilha_operandos(pilha_operandos **pilha)
+ *  @brief Desaloca pilha de operandos e seta cabecao da pilha para NULL.
+ *  @param pilha_operandos Ponteiro para ponteiro de pilha de operandos.
+ */
+ void desaloca_pilha_operandos(pilha_operandos**);
+
+  /**
+ *  @fn void empilha_operando(pilha_operandos **pilha, u4 op)
+ *  @brief Empilha operando na pilha com base em modelo last-in-first-out.
+ *  @param pilha_operandos Ponteiro para ponteiro de pilha de operandos.
+ *  @param op Operando a ser empilhado.
+ */
+ void empilha_operando(pilha_operandos**, u4);
+
+   /**
+ *  @fn u4 desempilha_operando(pilha_operandos **pilha)
+ *  @brief Desempilha operando da pilha com base em modelo last-in-first-out.
+ *  @param pilha_operandos Ponteiro para ponteiro de pilha de operandos.
+ *  @param op Operando a ser empilhado.
+ *  @return Operando desempilhado com base em modelo last-in-first-out.
+ */
+ u4 desempilha_operando(pilha_operandos**);
+
+#endif
