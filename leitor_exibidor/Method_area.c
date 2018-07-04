40632d3409b5fdf4f0989832137d3b07f0f3d15f
gustavocsousa <gustavo_csousa@hotmail.com>
2018-07-04 02:35:43 -0300

    Alterações em parte de Área de Métodos

---
+++ leitor_exibidor/Method_area.c
@@ -0,0 +1,45 @@
+/**
+*@file Method_area.case
+*@author Gustavo Costa 14/0142568
+*@author Andrei
+*@brief Implementação da área de métodos da JVM.
+*/
+
+#include "Method_area.h"
+
+int inicializa(ClassFile *classe, Pilha_frames *pilha) {
+  int result = 0;
+  for(int i = 0; i<classe->methods_count; i++) {
+    char *nome;
+    int local_nome = classe->constant_pool[i].utf8_info; //Ver esse indicacao de nome!!!
+    nome = getConstantUTF8CP(local_nome, classe);
+    if (!strcmp("<clinit>", nome) || !strcmp("<init>", nome)) { //verifica se e comeco do metodo
+      method_info *init = &classe->methods[i];
+
+    }
+  }
+}
+
+method_info* acessoMethod(ClassFile *classe, char *metodo) {
+
+  for (int i = 0; i < classe->methods_count; i++) {
+    int local_nome = classe->constant_pool[i].utf8_info; //Ver esse indicacao de nome!!!
+    u1 *nome;
+    nome  = (u1*) getConstantUTF8CP(local_nome, classe);
+    if(!strcmp(metodo, (char *) nome)){
+      return &classe->methods[i];
+    }
+  }
+  return NULL;
+}
+
+void ready(method_info *method, ClassFile *classe, Pilha_frames **pilha){
+  for(int i = 0; i<method->attributes_count; i++) {
+    if (method->attributes[i].tag == ATTRIBUTE_Code) {
+      if (method->attributes_count > 0) {
+        Frame *frame = init_frame(*method, classe->*constant_pool); //Ver isso!!!
+        *pilha = empilha_frame()
+      }
+    }
+  }
+}
---
+++ leitor_exibidor/Method_area.h
@@ -0,0 +1,39 @@
+/**
+*@file Method_area.h
+*@author Gustavo Costa 14/0142568
+*@author Andrei
+*@brief Biblioteca com funcoes implementadas para área de métodos
+*/
+
+#include "classfile.h"
+#include "pilhaFrames.h"
+
+#ifndef METHOD_AREA_H
+#define METHOD_AREA_H
+
+/**
+*@fn int inicializa(ClassFile *classe, Pilha_frames *pilha)
+*@brief Execucao dos metodos que iniciam a classe
+*@param Classe carregada
+*@param Pilha carregada
+*/
+int inicializa(ClassFile *classe, Pilha_frames *pilha);
+
+/**
+*@fn method_info* acessoMethod(ClassFile *classe, char *metodo)
+*@brief Funcao que avalia os metodos e retorna o metodo main da classe
+*@param Classe carregada
+*@param Nome do metodo desejado
+*/
+
+method_info* acessoMethod(ClassFile *classe, char *metodo);
+
+/**
+*@fn void ready(method_info *metodo, ClassFile *classe, Pilha_frames *pilha)
+*@brief Funcao de estado antes da execucao do metodo
+*@param Metodo a ser executado posteriormente
+*@param Classe carregada "classe"
+*@param Pilha_frames que contem e armazena o frame
+*/
+
+void ready(method_info *method, ClassFile *classe, Pilha_frames *pilha);
