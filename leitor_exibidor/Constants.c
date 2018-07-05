1bad97d522dfcec18da0de574d0c261fe2147afb
douglasshiro <douglasshiro@hotmail.com>
2018-07-05 17:23:35 -0300

    correcao constantes

--- leitor_exibidor/Constants.c
+++ leitor_exibidor/Constants.c
@@ -13,63 +13,63 @@ void nop (){
 
 void aconst_null(Frame *frame){
     u4 operando = 0;
-    empilha_operando(&(frame->Pilha_Operandos), valor);
-	
+    empilha_operando(&(frame->pilha_operandos), operando);
+
     return;
 };
 
 void iconst_m1(Frame *frame){
 	int32_t operando = -1;
-	
-	empilha_operando(&(frame->Pilha_Operandos), operando);
-	
+
+	empilha_operando(&(frame->pilha_operandos), operando);
+
 	return;
 }
 
 void iconst_0(Frame *frame){
 	u4 operando = 0;
-	
-	empilha_operando(&(frame->Pilha_Operandos), operando);
-	
+
+	empilha_operando(&(frame->pilha_operandos), operando);
+
 	return;
 }
 
 void iconst_1(Frame *frame){
 	u4 operando = 1;
-	
-	empilha_operando(&(frame->Pilha_Operandos), operando);
-	
+
+	empilha_operando(&(frame->pilha_operandos), operando);
+
 	return;
 }
 
 void iconst_2(Frame *frame){
 	u4 operando = 2;
-	
-	empilha_operando(&(frame->Pilha_Operandos), operando);
-	
+
+	empilha_operando(&(frame->pilha_operandos), operando);
+
 	return;
 }
 
 void iconst_3(Frame *frame){
 	u4 operando = 3;
-	
-	empilha_operando(&(frame->Pilha_Operandos), operando);
-	
+
+	empilha_operando(&(frame->pilha_operandos), operando);
+
 	return;
 }
 
 void iconst_4(Frame *frame){
 	u4 operando = 4;
-	
-	empilha_operando(&(frame->Pilha_Operandos), operando);
-	
+
+	empilha_operando(&(frame->pilha_operandos), operando);
+
 	return;
 }
 
 void iconst_5(Frame *frame){
 	u4 operando = 5;
-	
-	empilha_operando(&(frame->Pilha_Operandos), operando);
-	
+
+	empilha_operando(&(frame->pilha_operandos), operando);
+
 	return;
 }
